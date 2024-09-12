66a79e982d64fb8da4476ce4f3d0e32009a7cf2d
MuhammadArfi <151350795+MuhammadArfi@users.noreply.github.com>
2024-09-12 19:33:58 +0700

    UPLOAD3

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D80&reauth=1&qs_redir_cnt=1">
+		<meta http-equiv="refresh" content="0;url=/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D156&reauth=1&qs_redir_cnt=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D80&reauth=1&qs_redir_cnt=1";
+			window.location = "/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D156&reauth=1&qs_redir_cnt=1";
 		</script>
 
-		<p>You are being redirected to <a href="/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D80&reauth=1&qs_redir_cnt=1">/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D80&reauth=1&qs_redir_cnt=1</a></p>
+		<p>You are being redirected to <a href="/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D156&reauth=1&qs_redir_cnt=1">/wp-login.php?redirect_to=https%3A%2F%2Foxygen-qfk5nra26jl2l.oxygen-demo.qsandbox.org%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D156&reauth=1&qs_redir_cnt=1</a></p>
 	</body>
 </html>
